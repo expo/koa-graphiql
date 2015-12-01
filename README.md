af4f0188ebe4e0dd22d22515053a308eb893c032
James Ide <ide@jameside.com>
2015-11-30 19:29:46 -0800

    [1.0.0] Initial implementation

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # koa-graphiql
-Koa middleware to display GraphiQL, the interactive GraphQL UI
+
+Koa middleware to display [GraphiQL](https://github.com/graphql/graphiql). Designed for Koa 2.
+
+## Usage
+
+```sh
+npm i --save koa-graphiql
+```
+
+Add it to your Koa app. You may want to use router middleware if your app serves more than GraphiQL.
+
+```js
+import koaGraphiQL from 'koa-graphiql';
+
+router.get('/graphiql', koaGraphiQL(async (ctx) => {
+  return {
+    url,        // String of the base URL of the GraphQL endpoint
+    query,      // String to display in the query panel
+    result,     // Object to display in the result panel
+    variables,  // Object used to populate the "variables" panel
+  };
+}));
+```
---
+++ src/middleware.js
@@ -0,0 +1,126 @@
+/**
+ * @flow
+ */
+type Options = {
+  url: ?string;
+  query: ?string;
+  variables: ?Object;
+  result?: Object;
+};
+type GetOptions = (ctx: Object) => Promise<Options>;
+
+// Current latest version of GraphiQL
+const GRAPHIQL_VERSION = '0.4.4';
+
+export default function createGraphiQLMiddleware(getOptionsAsync: ?GetOptions) {
+  return async function middleware(ctx) {
+    let options = {};
+    if (getOptionsAsync) {
+      options = await getOptionsAsync(ctx);
+    }
+
+    let html = renderHtml(options);
+    ctx.body = html;
+    ctx.type = 'text/html';
+  }
+}
+
+/**
+ * See express-graphql for the original implementation
+ */
+function renderHtml(options: Options): string {
+  let url = options.url || '';
+  let queryString = options.query;
+  let variablesString = options.variables ?
+    JSON.stringify(options.variables, null, 2) :
+    null;
+  let resultString = options.result ?
+    JSON.stringify(data.result, null, 2) :
+    null;
+
+  // How to Meet Ladies
+  return (
+`<!DOCTYPE html>
+<html>
+<head>
+  <link href="//cdn.jsdelivr.net/graphiql/${GRAPHIQL_VERSION}/graphiql.css" rel="stylesheet" />
+  <script src="//cdn.jsdelivr.net/fetch/0.9.0/fetch.min.js"></script>
+  <script src="//cdn.jsdelivr.net/react/0.14.2/react.min.js"></script>
+  <script src="//cdn.jsdelivr.net/react/0.14.2/react-dom.min.js"></script>
+  <script src="//cdn.jsdelivr.net/graphiql/${GRAPHIQL_VERSION}/graphiql.min.js"></script>
+</head>
+<body>
+  <script>
+    // Collect the URL parameters
+    var parameters = {};
+    window.location.search.substr(1).split('&').forEach(function (entry) {
+      var eq = entry.indexOf('=');
+      if (eq >= 0) {
+        parameters[decodeURIComponent(entry.slice(0, eq))] =
+          decodeURIComponent(entry.slice(eq + 1));
+      }
+    });
+    // Produce a Location query string from a parameter object.
+    function locationQuery(params) {
+      return '?' + Object.keys(params).map(function (key) {
+        return encodeURIComponent(key) + '=' +
+          encodeURIComponent(params[key]);
+      }).join('&');
+    }
+    // Derive a fetch URL from the current URL, sans the GraphQL parameters.
+    var graphqlParamNames = {
+      query: true,
+      variables: true,
+      operationName: true
+    };
+    var otherParams = {};
+    for (var k in parameters) {
+      if (parameters.hasOwnProperty(k) && graphqlParamNames[k] !== true) {
+        otherParams[k] = parameters[k];
+      }
+    }
+    var fetchURL = ${JSON.stringify(url)} + locationQuery(otherParams);
+    // Defines a GraphQL fetcher using the fetch API.
+    function graphQLFetcher(graphQLParams) {
+      return fetch(fetchURL, {
+        method: 'post',
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(graphQLParams),
+        credentials: 'include',
+      }).then(function (response) {
+        return response.json();
+      });
+    }
+    // When the query and variables string is edited, update the URL bar so
+    // that it can be easily shared.
+    function onEditQuery(newQuery) {
+      parameters.query = newQuery;
+      updateURL();
+    }
+    function onEditVariables(newVariables) {
+      parameters.variables = newVariables;
+      updateURL();
+    }
+    function updateURL() {
+      history.replaceState(null, null, locationQuery(parameters));
+    }
+    // Render <GraphiQL /> into the body.
+    React.render(
+      React.createElement(GraphiQL, {
+        fetcher: graphQLFetcher,
+        onEditQuery: onEditQuery,
+        onEditVariables: onEditVariables,
+        query: ${JSON.stringify(queryString)},
+        response: ${JSON.stringify(resultString)},
+        variables: ${JSON.stringify(variablesString)}
+      }),
+      document.body
+    );
+  </script>
+</body>
+</html>`
+  );
+}
